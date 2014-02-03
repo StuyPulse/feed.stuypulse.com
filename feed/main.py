3b9cfd9c6836f2889776110f4a650f5f99dd1190
ChesleyTan <ChesleyTan97@gmail.com>
2014-02-02 20:57:46 -0500

    Implement login/logout and redirects for admin and regular users

--- feed/main.py
+++ feed/main.py
@@ -4,12 +4,20 @@ from google.appengine.api import users
 
 class MainHandler(webapp2.RequestHandler):
 
-    def get(self):
-        # Set the cross origin resource sharing header to allow AJAX
-        self.response.headers.add_header("Access-Control-Allow-Origin", "*")
-
-        template = JINJA_ENVIRONMENT.get_template('index.html')
-        self.response.write(template.render({}))
+	def get(self):
+		# Set the cross origin resource sharing header to allow AJAX
+		self.response.headers.add_header("Access-Control-Allow-Origin", "*")
+		user = users.get_current_user()
+		if user:
+			if users.is_current_user_admin():
+				self.redirect("/admin")
+			template_values = {
+				'logout_url': users.create_logout_url("/")
+			}
+			template = JINJA_ENVIRONMENT.get_template('hangouts.html')
+			self.response.write(template.render(template_values))
+		else:
+			self.redirect(users.create_login_url("/"))
 
 application = webapp2.WSGIApplication([
     ('/.*', MainHandler)

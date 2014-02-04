13b41df310c25cfef4cba7cfcce7ae766da88941
Danny Qiu <dqiu55@gmail.com>
2014-02-04 17:54:23 -0500

    Remove logout link from homepage

--- feed/main.py
+++ feed/main.py
@@ -11,15 +11,15 @@ class MainHandler(webapp2.RequestHandler):
     def get(self):
         # Set the cross origin resource sharing header to allow AJAX
         self.response.headers.add_header("Access-Control-Allow-Origin", "*")
+
         user = users.get_current_user()
         if users.is_current_user_admin():
             self.redirect("/admin")
         template_values = {
-            'logout_url': users.create_logout_url("/"),
             'hangout_exists': not HangoutUrl.get_by_id(694) is None,
             'hangout_link': '' if HangoutUrl.get_by_id(694) is None else HangoutUrl.get_by_id(694).content
         }
-        template = JINJA_ENVIRONMENT.get_template('hangouts.html')
+        template = JINJA_ENVIRONMENT.get_template('index.html')
         self.response.write(template.render(template_values))
 
 application = webapp2.WSGIApplication([

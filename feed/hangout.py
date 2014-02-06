f7ee50239e9cf7d239d62de5cb7a270670ba2eb2
Danny Qiu <dqiu55@gmail.com>
2014-02-06 18:39:50 -0500

    Only update datastore when link exists in request

--- feed/hangout.py
+++ feed/hangout.py
@@ -6,13 +6,15 @@ class HangoutHandler(webapp2.RequestHandler):
     def post(self):
         self.response.headers.add_header("Access-Control-Allow-Origin", "*")
         hangout = self.request.get('hangoutUrl')
-        hangoutUrl = HangoutUrl(id=1)
-        hangoutUrl.content = hangout
-        hangoutUrl.put()
+        if hangout != '':
+            hangoutUrl = HangoutUrl(id=1)
+            hangoutUrl.content = hangout
+            hangoutUrl.put()
         youtube = self.request.get('youtubeId')
-        youtubeUrl = HangoutUrl(id=2)
-        youtubeUrl.content = youtube
-        youtubeUrl.put()
+        if youtube != '':
+            youtubeUrl = HangoutUrl(id=2)
+            youtubeUrl.content = youtube
+            youtubeUrl.put()
 
 application = webapp2.WSGIApplication([
     ('/hangout', HangoutHandler)

114c84609c429a21e303c46a5bcf5c436c7a7cc3
Danny Qiu <dqiu55@gmail.com>
2014-02-07 22:36:55 -0500

    Secure cron job

--- feed/hangout.py
+++ feed/hangout.py
@@ -5,14 +5,6 @@ from google.appengine.ext import ndb
 
 class HangoutHandler(webapp2.RequestHandler):
 
-    def get(self):
-        hangout = HangoutUrl.get_by_id(1)
-        youtube = HangoutUrl.get_by_id(2)
-        diff = datetime.now() - hangout.time
-        if diff.seconds > 240:
-            ndb.Key(HangoutUrl, 1).delete()
-            ndb.Key(HangoutUrl, 2).delete()
-
     def post(self):
         self.response.headers.add_header("Access-Control-Allow-Origin", "*")
         hangoutUrl = HangoutUrl.get_by_id(1)
@@ -26,6 +18,17 @@ class HangoutHandler(webapp2.RequestHandler):
         hangoutUrl.time = datetime.now()
         hangoutUrl.put()
 
+class HangoutKiller(webapp2.RequestHandler):
+
+    def get(self):
+        hangout = HangoutUrl.get_by_id(1)
+        youtube = HangoutUrl.get_by_id(2)
+        diff = datetime.now() - hangout.time
+        if diff.seconds > 240:
+            ndb.Key(HangoutUrl, 1).delete()
+            ndb.Key(HangoutUrl, 2).delete()
+
 application = webapp2.WSGIApplication([
-    ('/hangout', HangoutHandler)
+    ('/hangout', HangoutHandler),
+    ('/hangout_kill', HangoutKiller)
 ], debug=True)
